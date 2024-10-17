59c55827ad708d66887eed20eae31a0ef1112b4f
Nuguri <nuguri.finekesco@gmail.com>
2024-10-17 17:58:16 +0900

    Creating Stopwatch Form

---
+++ StopWatchSample/StopWatchSample/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace StopWatchSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(13, 74);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(94, 32);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "START";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(113, 74);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(94, 32);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "STOP";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Location = new System.Drawing.Point(213, 74);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(94, 32);
+            this.button_Reset.TabIndex = 2;
+            this.button_Reset.Text = "RESET";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00:00.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 121);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form1";
+            this.Text = "STOPWATCH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ StopWatchSample/StopWatchSample/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace StopWatchSample
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch mStopwatch;
+        private Timer mStopWatchTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            mStopwatch = new Stopwatch();
+            mStopWatchTimer = new Timer();
+            mStopWatchTimer.Interval = 100;
+            mStopWatchTimer.Tick += Timer_Tick;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = mStopwatch.Elapsed;
+            label1.Text = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+        }
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            mStopwatch.Start();
+            mStopWatchTimer.Start();
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            mStopwatch.Stop();
+            mStopWatchTimer.Stop();
+        }
+
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            mStopwatch.Reset();
+            label1.Text = "00:00:00.00"; // 초기 상태로 리셋
+        }
+    }
+}
